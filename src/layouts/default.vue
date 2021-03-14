b90e55ab126e68c3474d09cb9561a50ff57d7c5b
Benjamin Atkin <ben@resources.co>
2021-03-14 14:02:06 -0400

    feat: make the splitter's draggable area larger than the gap between views ↔️

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
-import { onMounted } from 'vue';
-import Split from 'split-grid';
+import { onMounted } from 'vue'
+import Split from 'split-grid'
 
 onMounted(() => {
   Split({
@@ -8,37 +8,51 @@ onMounted(() => {
       track: 1,
       element: document.querySelector('.view-split'),
     }],
+    columnMinSizes: 0,
   })
 })
 </script>
 
-<style type="text/css">
-.view-container {
-  display: grid;
-  grid-template-columns: 1fr 10px 1fr;
-  height: 100vh;
-}
-.view-split {
-  cursor: ew-resize;
-}
-.view-split-bar {
-  width: 2px;
-}
-.view-split:hover .view-split-bar, .view-split:active .view-split-bar {
-  width: 4px;
-}
-</style>
-
 <template>
-  <div class="view-container">
-    <main class="view-a px-4 py-10 text-center text-gray-700 dark:text-gray-200">
+  <div class="view-container text-gray-700 dark:text-gray-200">
+    <div class="overflow-x-auto">
+      Tab 1
+    </div>
+    <div class="overflow-x-auto">
+      Preview
+    </div>
+    <main class="view-a overflow-auto px-4 py-10 text-center">
       <router-view />
     </main>
-    <div class="view-split flex justify-center">
-      <div class="view-split-bar h-full bg-gray-200 dark:bg-gray-700"></div>
-    </div>
-    <div class="view-b px-4 py-10 text-center text-gray-700 dark:text-gray-200">
+    <div class="view-b overflow-auto px-4 py-10 text-center">
       <Footer />
+    </div>
+    <div class="view-split">
+      <div class="view-split-bar h-full" />
     </div>
   </div>
 </template>
+
+<style type="text/css">
+.view-container {
+  display: grid;
+  grid-template-columns: 1fr 2px 1fr;
+  grid-template-rows: auto 1fr;
+  height: 100vh;
+}
+.view-split {
+  position: relative;
+  @apply bg-gray-200 hover:bg-gray-400 dark:bg-gray-700 dark:hover:bg-gray-500;
+  grid-column: 2;
+  grid-row: 1 / span 2;
+}
+.view-split-bar {
+  cursor: ew-resize;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: -10px;
+  right: -10px;
+  background: transparent;
+}
+</style>
