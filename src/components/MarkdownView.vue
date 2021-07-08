4a902a76f3890c41d6e032d6ad1ee7435c238857
Benjamin Atkin <ben@resources.co>
2021-07-07 17:43:43 -0700

    style: fix light code highlighting background 💅

--- src/components/MarkdownView.vue
+++ src/components/MarkdownView.vue
@@ -7,6 +7,7 @@ import xml from "highlight.js/lib/languages/xml";
 import css from "highlight.js/lib/languages/css";
 import javascript from "highlight.js/lib/languages/javascript";
 import typescript from "highlight.js/lib/languages/typescript";
+import json from "highlight.js/lib/languages/json";
 import highlight from "markdown-it-highlightjs/core";
 
 hljs.registerLanguage("markdown", markdown);
@@ -14,6 +15,7 @@ hljs.registerLanguage("xml", xml);
 hljs.registerLanguage("css", css);
 hljs.registerLanguage("javascript", javascript);
 hljs.registerLanguage("typescript", typescript);
+hljs.registerLanguage("json", json);
 
 const md = MarkdownIt().use(highlight, { hljs });
 
