1577ff9aa2220f8e22260610cf48273837a652b0
Benjamin Atkin <ben@resources.co>
2021-10-06 10:33:00 -0400

    🌛 update deps & fix dark mode colors

---
+++ windi.config.js
@@ -0,0 +1,21 @@
+import { defineConfig } from 'vite-plugin-windicss'
+import colors from 'windicss/colors'
+import typography from 'windicss/plugin/typography'
+
+export default defineConfig({
+  darkMode: 'class',
+  plugins: [
+    typography({dark: true}),
+  ],
+  theme: {
+    extend: {
+      typography: {
+        DEFAULT: {
+          css: {
+            maxWidth: '65ch',
+          },
+        },
+      },
+    },
+  },
+})
