b8435984e2d1e40c59cf75ee97dc3699172b4371
Benjamin Atkin <ben@resources.co>
2022-11-03 10:59:58 -0700

    feat: when hovering over the tabs while previewing, show at full opacity 💅

--- src/components/TabArea.vue
+++ src/components/TabArea.vue
@@ -3,7 +3,17 @@ const { active } = defineProps({ active: Boolean })
 </script>
 
 <template>
-  <div class="flex flex-grow" :class="active ? '' : 'opacity-30'">
+  <div class="flex flex-grow root" :class="active ? '' : 'inactive'">
     <slot />
   </div>
 </template>
+
+<style>
+  div.root.inactive {
+    @apply opacity-30;
+  }
+
+  div.root.inactive:hover {
+    @apply opacity-100;
+  }
+</style>
