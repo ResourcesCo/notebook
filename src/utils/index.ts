a76362b3bc80790168040c66baca0eb2708d6178
Benjamin Atkin <ben@resources.co>
2022-11-08 17:04:29 -0800

    refactor: remove old in-project test view

--- src/utils/index.ts
+++
@@ -1,1 +0,0 @@
-export { join as joinPath, split as splitPath } from './path-string'
--- src/utils/path-string/join.ts
+++
@@ -1,3 +0,0 @@
-export default function join(path: string[]): string {
-  return path.map(s => s.includes('/') ? JSON.stringify(s) : s).join('/')
-}
