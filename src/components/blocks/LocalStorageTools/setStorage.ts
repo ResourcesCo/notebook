b9193f79f11ef478331cb9d5190ed1fa1dbdfb83
Benjamin Atkin <ben@resources.co>
2022-11-12 23:59:20 -0800

    feat: begin setStorage

---
+++ src/components/blocks/LocalStorageTools/setStorage.ts
@@ -0,0 +1,11 @@
+import type { StorageExport, StoragesExport } from "./getStorage"
+
+function importStorage(data: StorageExport, storage: Storage) {
+
+}
+
+export default function setStorage(data: StoragesExport) {
+  for (const storage of ['local', 'session']) {
+    
+  }
+}
