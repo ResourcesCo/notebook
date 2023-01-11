6922cc2ae57331d27d41be77cc845a02f40a40ce
Benjamin Atkin <ben@resources.co>
2023-01-10 22:24:51 -0800

    add vite-plugin-pwa back and copy files from initial build before doing main build

--- src/app/view/main.ts
+++ src/app/view/main.ts
@@ -6,7 +6,12 @@ import "../../store"
 import '@unocss/reset/tailwind.css'
 import "../../styles/main.css"
 import "uno.css"
+import { registerSW } from 'virtual:pwa-register'
 
 const params = new URLSearchParams(window.location.search)
 const app = createApp(App, {params})
 app.mount("#app")
+
+if (import.meta.env.PROD) {
+  registerSW()
+}
